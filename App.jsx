d3aa4a8f6c29ee1da17b2a31f27e620e83836997
Lucas <lucas@agtlucas.com>
2015-09-24 07:23:41 -0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,15 +1,16 @@
 // App Component - represents the whole app
 App = React.createClass({
-  getTasks() {
-    return [
-      { _id: 1, text: 'Task 1' },
-      { _id: 2, text: 'Task 2' },
-      { _id: 3, text: 'Task 3' }
-    ]
+
+  mixins: [ReactMeteorData],
+
+  getMeteorData() {
+    return {
+      tasks: Tasks.find({}).fetch()
+    }
   },
 
   renderTasks() {
-    return this.getTasks().map((task) => {
+    return this.data.tasks.map((task) => {
       return <Task key={task._id} task={task} />;
     });
   },
